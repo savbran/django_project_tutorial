9f0a4dc87f660b428e6ac25dbd7f04a8cc77566b
Saverio Brancaccio <saverio.brancaccio@gmail.com>
2018-05-08 12:44:35 +0200

    addee3d requirements and updated stuff

--- web_polls/views.py
+++ web_polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 
 
+# A view in Django is a function or amethon in case of class-based views
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
